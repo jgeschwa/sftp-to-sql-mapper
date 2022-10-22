07a7b6d403aaac6e79509a60a408d2b36be2162c
Jan Geschwandtner <geschwandtner.jan@gmail.com>
2022-10-22 18:42:29 +0200

    Connect to sftp server and read files

--- src/main/java/com/jgeschwa/fileimporter/FileImporterApplication.java
+++ src/main/java/com/jgeschwa/fileimporter/FileImporterApplication.java
@@ -1,13 +1,77 @@
 package com.jgeschwa.fileimporter;
 
+import com.jcraft.jsch.ChannelSftp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.integration.file.filters.AcceptAllFileListFilter;
+import org.springframework.integration.file.remote.session.CachingSessionFactory;
+import org.springframework.integration.file.remote.session.SessionFactory;
+import org.springframework.integration.handler.advice.ExpressionEvaluatingRequestHandlerAdvice;
+import org.springframework.integration.sftp.inbound.SftpStreamingMessageSource;
+import org.springframework.integration.sftp.session.DefaultSftpSessionFactory;
+import org.springframework.integration.sftp.session.SftpRemoteFileTemplate;
+import org.springframework.integration.transformer.StreamTransformer;
+import org.springframework.messaging.MessageHandler;
+
+import java.io.InputStream;
 
 @SpringBootApplication
 public class FileImporterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FileImporterApplication.class, args);
+    }
+
+    @Bean
+    public SessionFactory<ChannelSftp.LsEntry> sftpSessionFactory() {
+        DefaultSftpSessionFactory factory = new DefaultSftpSessionFactory(true);
+        factory.setHost("localhost");
+        factory.setPort(8092);
+        factory.setUser("foo");
+        factory.setPassword("pass");
+        factory.setAllowUnknownKeys(true);
+        return new CachingSessionFactory<>(factory);
+    }
+
+    @Bean
+    @InboundChannelAdapter(channel = "stream")
+    public MessageSource<InputStream> ftpMessageSource() {
+        SftpStreamingMessageSource messageSource = new SftpStreamingMessageSource(template());
+        messageSource.setRemoteDirectory("upload/");
+        messageSource.setFilter(new AcceptAllFileListFilter<>());
+        messageSource.setMaxFetchSize(1);
+        return messageSource;
+    }
+
+    @Bean
+    @Transformer(inputChannel = "stream", outputChannel = "data")
+    public org.springframework.integration.transformer.Transformer transformer() {
+        return new StreamTransformer("UTF-8");
+    }
+
+    @Bean
+    public SftpRemoteFileTemplate template() {
+        return new SftpRemoteFileTemplate(sftpSessionFactory());
+    }
+
+    @ServiceActivator(inputChannel = "data", adviceChain = "after")
+    @Bean
+    public MessageHandler handle() {
+        return System.out::println;
+    }
+
+    @Bean
+    public ExpressionEvaluatingRequestHandlerAdvice after() {
+        ExpressionEvaluatingRequestHandlerAdvice advice = new ExpressionEvaluatingRequestHandlerAdvice();
+//        advice.setOnSuccessExpressionString(
+//                "@template.remove(headers['file_remoteDirectory'] + headers['file_remoteFile'])");
+        advice.setPropagateEvaluationFailures(true);
+        return advice;
     }
 
 }
