56bcfcc4796994d058d90a0b7f9e2d09f99d6391
Jan Geschwandtner <geschwandtner.jan@gmail.com>
2022-10-22 17:47:09 +0200

    Initial commit

---
+++ src/test/java/com/jgeschwa/fileimporter/FileImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgeschwa.fileimporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileImporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
